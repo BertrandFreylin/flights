489abf27cb67bf18a04f147f262bfe94b0612a8a
BertrandF26 <bfreylin@gmail.com>
2016-07-17 14:34:08 +0200

    Better

--- webservices/list_compagnie.php
+++ webservices/list_compagnie.php
@@ -7,7 +7,7 @@
 	include("../bdd/connexion_bdd.php");
 	
 
-	$query = "SELECT code, description FROM carriers LIMIT 5";
+	$query = "SELECT code, description FROM carriers LIMIT 20";
 
 	$result = mysqli_query($conn, $query);
 
